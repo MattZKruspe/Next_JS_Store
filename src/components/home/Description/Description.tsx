1e87ba37c64c3c85fe8c885a4426c5508cbd883d
MattZKruspe <matthewmarquez2003@gmail.com>
2024-03-01 14:28:08 -0600

    Agregando Archivos Estaticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -2,7 +2,8 @@ export const Description = () => {
 
     return (
         <section>
-            <h1>Description</h1>
+            <img src="/public/images/description.jpeg" alt="products marketplace" />
+            <p>Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar el texto fin</p>
         </section>        
     )
 }
