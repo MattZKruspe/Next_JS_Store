c02ca4a4a0f1911dad7c65f28f68e27ec6bf447d
MattZKruspe <matthewmarquez2003@gmail.com>
2024-04-18 22:41:00 -0600

    3_3 Obtener Informacion API

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,8 +1,17 @@
-"use client"
 
-export const MainProducts = () => {
+const getProducts = async () => {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`,{
+        headers: new Headers ({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+        })
+    })
+    const data = await response.json()
+    return data
+}
 
-    console.log("variable de entorno",process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME);
+export const MainProducts = async () => {
+    const products = await getProducts()
+    console.log(products)
 
     return(
 
